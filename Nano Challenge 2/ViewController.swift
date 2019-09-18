cdc3bf93f1216ddec3536ab2382b9fb8c2ebc909
Raiman <wijayakaleb@gmail.com>
2019-09-18 15:47:35 +0700

    Initial Commit

---
+++ Nano Challenge 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nano Challenge 2
+//
+//  Created by Kaleb Wijaya on 18/09/19.
+//  Copyright © 2019 Kaleb Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
